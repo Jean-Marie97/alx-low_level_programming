050a00ff661eb317c78f6f329b112afaf363a140
Jean-Marie97 <yankeyalexander@gmail.com>
2022-06-14 19:20:12 -0700

    done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,33 +12,13 @@
  */
 
 int main(void)
-int i, j, k, s;
-char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char p[58];
-srand(time(NULL));
-while (s != 2772)
+
 {
-i = k = s = 0;
-while ((2772 - 122) > s)
+srand((unsigned int)(time(NULL)));
+int index = 0;
+char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (index = 0; index < 12; index++)
 {
-j = rand() % 62;
-p[i] = c[j];
-s += c[j];
-i++;
+printf("%c", characters[rand() % (sizeof(characters) - 1)]);
 }
-while (c[k])
-{
-if (c[k] == (2772 - s))
-{
-p[i] = c[k];
-s += c[k];
-i++;
-break;
-}
-k++;
-}
-}
-p[i] = '\0';
-printf("%s", p);
-return (0);
 }
