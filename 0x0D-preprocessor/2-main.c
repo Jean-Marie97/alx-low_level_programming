6b80121092cda6bb39f4cce68dc3ac823841e6f2
Jean-Marie97 <yankeyalexander@gmail.com>
2022-07-05 10:46:21 -0700

    Trial1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print name
+* Return: 0
+**/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_MACRO
+#define FUNCTION_MACRO
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
+
