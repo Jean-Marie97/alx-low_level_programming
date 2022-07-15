41662e73a11847515dac92e708102db5be721cc0
Jean-Marie97 <yankeyalexander@gmail.com>
2022-07-15 15:21:02 -0700

    trial2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,22 @@
-#include "lists.h"
+#include <stdio.h>
 
-void print_tortoise(void)__attribute__ ((constructor));
+
+
+void __attribute__((constructor)) print_first(void);
+
+
 
 /**
- * print_tortoise - that prints before the main function is executed.
- * Return: value
+ * print_first - Prints a string before the
+ * main function is executed.
  */
 
 
-void print_tortoise(void)
+
+void print_first(void)
+
 {
+
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
