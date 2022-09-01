e17ca409f1d63e031d4bc755ef6d61abb1d7818e
Jean-Marie97 <yankeyalexander@gmail.com>
2022-09-01 11:25:27 -0700

    first try

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+
+
+/**
+
+ * print_dlistint - prints all the elements of a list
+
+ * @h: head
+
+ * Return: the number of nodes
+
+ */
+
+
+
+size_t print_dlistint(const dlistint_t *h)
+
+{
+
+size_t nod = 0;
+
+
+
+while (h)
+
+{
+
+nod++;
+
+printf("%d\n", h->n);
+
+h = h->next;
+
+}
+
+return (nod);
+
+}
+
+
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+
+
+/**
+
+ * add_dnodeint - adds a new node at the beginning
+
+ * @head: head
+
+ * @n: value of node
+
+ * Return: new element or NULL
+
+ */
+
+
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+
+{
+
+dlistint_t *d_node;
+
+
+
+d_node = malloc(sizeof(dlistint_t));
+
+
+
+if (d_node == NULL)
+
+return (NULL);
+
+
+
+d_node->n = n;
+
+d_node->next = (*head);
+
+d_node->prev = NULL;
+
+
+
+if (*head != NULL)
+
+(*head)->prev = d_node;
+
+
+
+*head = d_node;
+
+
+
+return (d_node);
+
+}
